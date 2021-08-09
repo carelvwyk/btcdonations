83053831de30214bc105fedda3496b2f3d9923de
CarelvWyk <cpjvanwyk@gmail.com>
2021-08-09 15:03:39 +0200

    Update live

--- btcdonations.js
+++ btcdonations.js
@@ -5,13 +5,27 @@ var address = 'bc1q6zzx9gq4343rz8r5x95mdzw3qvc6rw7cdc50ur', // Set Donation Addr
     goal = 0.001, // Your goal in BTC
     donorReference = 'SparkleVerse Testing Donation'; // Message that will appear in donor wallet
 
+function updateComponents(received) {
+    let b2s = Math.pow(10, 8); // BTC to Sats
+    let percentage = received / (goal * b2s) * 100;
+
+    let width = percentage > 100 ? 100 : percentage;
+    $('#donationbar').animate({
+        width: width + "%"
+    }, 2000);
+
+    $('#percentagebtc').html(percentage | 0);
+    $('#goalbtc').html(goal);
+    $('#receivedbtc').html((received / b2s).toFixed(8));
+}
+
 async function getReceived(address) {
     try {
         let blockchainUrl = 'https://blockchain.info/q/getreceivedbyaddress/' + address + '?cors=true';
         let data = await window.fetch(blockchainUrl, {
             mode: 'cors'
         }).then(response => response.json())
-            .then(received => { return received });
+            .then(received => { updateComponents(received); });
         return data;
     } catch (err) {
         console.log(err);
@@ -24,17 +38,5 @@ $('#donateButton').attr('href', donationUri);
 $('#donateQR').attr('src', 'https://chart.googleapis.com/chart?cht=qr&chs=300x300&chl='+encodeURIComponent(donationUri));
 
 // Get Received amount and update components:
-getReceived(address).then((received) => {
-    let b2s = Math.pow(10, 8); // BTC to Sats
-    let percentage = received / (goal * b2s) * 100;
-
-    let width = percentage > 100 ? 100 : percentage;
-    console.log(percentage);
-    $('#donationbar').animate({
-        width: width + "%"
-    }, 2000);
-
-    $('#percentagebtc').html(percentage | 0);
-    $('#goalbtc').html(goal);
-    $('#receivedbtc').html((received / b2s).toFixed(8));
-});
+getReceived(address);
+setInterval(function() { getReceived(address); }, 5000); // Update every 5 seconds
